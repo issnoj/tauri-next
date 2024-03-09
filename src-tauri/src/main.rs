bd7d3b9fb00c03eadd01b81edb1d2b023fd74eba
issnoj <issei.nojiri@gmail.com>
2024-03-09 13:22:54 +0900

    event, window

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,17 +1,51 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// use std::path::PathBuf;
-// use tauri::PathResolver;
+use tauri::{CustomMenuItem, Manager, Menu, MenuItem, Submenu, WindowBuilder};
 
 mod cmd;
 
 fn main() {
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let close = CustomMenuItem::new("close".to_string(), "Close");
+    let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
+    let menu = Menu::new()
+        .add_native_item(MenuItem::Copy)
+        .add_item(CustomMenuItem::new("hide", "Hide"))
+        .add_submenu(submenu);
+
     tauri::Builder::default()
+        .menu(menu)
+        .on_menu_event(|event| {
+            match event.menu_item_id() {
+                "quit" => {
+                    std::process::exit(0);
+                }
+                "close" => {
+                    event.window().close().unwrap();
+                }
+                _ => {}
+            }
+        })
         .invoke_handler(tauri::generate_handler![
             cmd::get_home_dir,
             cmd::get_entries
         ])
+        .setup(|app| {
+            app.listen_global("front-to-back", |event| {
+                println!("got: {:?}", event.payload().unwrap())
+            });
+
+            let app_handle = app.app_handle();
+            std::thread::spawn(move || loop {
+                app_handle
+                    .emit_all("back-to-front", "ping front".to_string())
+                    .unwrap();
+                std::thread::sleep(std::time::Duration::from_secs(1))
+            });
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/components/other.tsx
+++ src/components/other.tsx
@@ -1,16 +1,63 @@
 'use client';
 
 import { open } from '@tauri-apps/api/dialog';
+import { emit, listen } from '@tauri-apps/api/event';
+import { WebviewWindow } from '@tauri-apps/api/window';
+import { useEffect, useRef } from 'react';
+import type { UnlistenFn } from '@tauri-apps/api/helpers/event';
 
 export const Other = () => {
+  const emitMessageInput = useRef<HTMLInputElement>(null);
+
   const handleClickOpen = () => {
     open().then((files) => console.log(files));
   };
 
+  const handleClickEmitMessage = () => {
+    if (!emitMessageInput.current) {
+      return;
+    }
+    emit('front-to-back', emitMessageInput.current.value);
+  };
+
+  const handleClickOpenWindow = () => {
+    const webview = new WebviewWindow('window');
+    webview.emit('front-to-back', 'new window');
+  };
+
+  useEffect(() => {
+    let unlisten: UnlistenFn;
+
+    async function f() {
+      unlisten = await listen('back-to-front', (event) => {
+        console.log(`back-to-front ${event.payload} ${new Date()}`);
+      });
+    }
+
+    f();
+
+    return () => {
+      if (unlisten) {
+        unlisten();
+      }
+    };
+  }, []);
+
   return (
     <div>
       <h1>Other</h1>
-      <button onClick={handleClickOpen}>open</button>
+      <div className={'flex flex-col gap-4'}>
+        <div>
+          <button onClick={handleClickOpen}>open</button>
+        </div>
+        <div className={'flex gap-4'}>
+          <button onClick={handleClickOpenWindow}>open window</button>
+        </div>
+        <div className={'flex gap-4'}>
+          <input type={'text'} ref={emitMessageInput} />
+          <button onClick={handleClickEmitMessage}>emit message</button>
+        </div>
+      </div>
     </div>
   );
 };
